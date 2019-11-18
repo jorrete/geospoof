21ab22726c087f020687b5afcaf257ea0de0b48c
Jorge Rodríguez-Flores Esparza <jorrete@gmail.com>
2019-11-18 19:14:28 +0100

    Remove example

--- example/htdocs/panel.js
+++
@@ -1,6 +0,0 @@
-import {StatusApp} from '../../app/modules/status';
-
-new StatusApp({
-    url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    coords: [-0.3303, 39.46118],
-});
--- example/htdocs/test.js
+++
@@ -1,8 +0,0 @@
-/* global QUnit */
-import index from '..';
-
-QUnit.module('index', function() {
-    QUnit.test('test', function( assert ) {
-        assert.ok('foo' in index, 'foo founded');
-    });
-});
